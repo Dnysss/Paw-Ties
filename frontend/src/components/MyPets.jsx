ed48b1cf0987cc20ace51a56d5d2a7f81bd3866c
Dnysss <denysantos2014@hotmail.com>
2024-06-03 14:19:17 -0300

    rendering pets in mypets

--- frontend/src/components/MyPets.jsx
+++ frontend/src/components/MyPets.jsx
@@ -1,16 +1,38 @@
 import Footer from "./Footer";
 import Nav from "./Nav";
+import Message from "./Message";
 import { useModal } from "./modals/ModalContext";
-import { pets } from "../data";
 
+import { FaCheckCircle } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { MdEditSquare } from "react-icons/md";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+
+
+import useFlashMessage from "../../hooks/useFlashMessage";
+
+import api from "../../utils/api";
+import { Link } from "react-router-dom";
 
 function MyPets() {
   const [pets, setPets] = useState([]);
   const { openModal } = useModal();
-  const { name, img } = pets;
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api
+      .get("/pets/mypets", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setPets(response.data.pets);
+      });
+  }, [token]);
+
   return (
     <>
       <Nav bgColorClass="bg-white" />
@@ -32,15 +54,15 @@ function MyPets() {
       </div>
 
       <div className="container mx-auto p-4 min-h-svh max-w-[1300px]">
-        {pets.map((pet, index) => {
+        {pets.map((pet) => {
           return (
             <div
-              key={index}
+              key={pet._id}
               className="relative flex flex-col items-center border border-solid border-gray-200 rounded-2xl transition-all duration-500 md:flex-row shadow-md mb-10"
             >
               <div className="block overflow-hidden md:w-52 h-48">
                 <img
-                  src={pet.img}
+                  src={pet.images[0]}
                   alt="Card image"
                   className="h-full rounded-2xl object-cover"
                 />
@@ -52,15 +74,27 @@ function MyPets() {
                 </h4>
 
                 <div className="flex flex-wrap w-full sm:w-auto justify-center">
-                  <button className="flex items-center bg-[#002A48] shadow-sm rounded-full py-2 px-5 text-xs text-white font-semibold mr-2 mb-2 sm:mb-0 hover:bg-blue-900 transition duration-300">
-                    <div className="mr-1">Edit</div>
-                    <MdEditSquare className="w-4 h-4" />
-                  </button>
+                  {pet.available ? (
+                    <>
+                      {pet.adopter && (
+                        <Link to="/" className="flex items-center bg-[#002A48] shadow-sm rounded-full py-2 px-5 text-xs text-white font-semibold mr-2 mb-2 sm:mb-0 hover:bg-blue-900 transition duration-300">
+                          <div className="mr-1">Conclir adoção</div>
+                          <FaCheckCircle className="w-4 h-4" />
+                        </Link>
+                      )}
+                      <button className="flex items-center bg-[#002A48] shadow-sm rounded-full py-2 px-5 text-xs text-white font-semibold mr-2 mb-2 sm:mb-0 hover:bg-blue-900 transition duration-300">
+                        <div className="mr-1">Edit</div>
+                        <MdEditSquare className="w-4 h-4" />
+                      </button>
 
-                  <button className="flex items-center bg-red-500 shadow-sm rounded-full py-2 px-5 text-xs text-white font-semibold mb-2 sm:mb-0 hover:bg-red-400 transition duration-300">
-                    <div className="mr-1">Delete</div>
-                    <MdDelete className="w-4 h-4" />
-                  </button>
+                      <button className="flex items-center bg-red-500 shadow-sm rounded-full py-2 px-5 text-xs text-white font-semibold mb-2 sm:mb-0 hover:bg-red-400 transition duration-300">
+                        <div className="mr-1">Delete</div>
+                        <MdDelete className="w-4 h-4" />
+                      </button>
+                    </>
+                  ) : (
+                    <p>Pet already adopted</p>
+                  )}
                 </div>
               </div>
             </div>
